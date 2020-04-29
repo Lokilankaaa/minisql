24727f1e459c27176b39b891b2374507f5cb59bd
Lokilanka <xw7771428@gmail.com>
2020-04-29 16:46:38 +0800

    initialize

---
+++ cmake-build-debug/CMakeFiles/minisql.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minisql.dir/main.cpp.o"
+  "minisql"
+  "minisql.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minisql.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
