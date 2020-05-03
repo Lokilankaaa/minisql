45af7532d5f6d238a57fcc65393411da54e64761
Lokilanka <xw7771428@gmail.com>
2020-05-03 10:55:07 +0800

    backup

--- main.cpp
+++ main.cpp
@@ -2,3 +2,12 @@
 // Created by 薛伟 on 2020/4/29.
 //
 
+#include "src/interpreter.h"
+#include <iostream>
+
+int main(){
+    Interpreter i;
+    string sql="create table test(id varchar(10), idd varchar(20));create table test2(id varchar(10), idd varchar(20));";
+    i.get_script(sql);
+    return 0;
+}
--- src/Catalog_manager.h
+++ src/Catalog_manager.h
@@ -4,5 +4,26 @@
 
 #ifndef MINISQL_CATALOG_MANAGER_H
 #define MINISQL_CATALOG_MANAGER_H
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+struct Table{
+    string table_name;
+    map<string, string> attrbutes;
+    string primary_key;
+};
+
+struct Database {
+    string database_name;
+    vector<Table> tables;
+};
+
+class catalog_manager {
+protected:
+
+};
 
 #endif //MINISQL_CATALOG_MANAGER_H
